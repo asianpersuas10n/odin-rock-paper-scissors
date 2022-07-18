c299d37fa3292b27c25685a7ed1615db8e6ded25
nick <nicholasjpenaranda@gmail.com>
2022-07-18 03:04:40 -0500

    create round based system goes to five rounds

--- rps.js
+++ rps.js
@@ -1,27 +1,50 @@
-let playerSelection = prompt("Chose Rock, Paper, or Scissors: ");
+ function game() {
+    let win = 0;
+    let loss = 0;
+    let tie = 0;
 
-let playerSelectionLower = playerSelection.toLowerCase();
+      for (let i = 0; i < 5; i++) { 
+        function gameLogic() {
 
-function getComputerChoice() {
-    const randNum = Math.floor(Math.random() * (3 - 1) + 1);
-    if (randNum === 1) {
-        return "rock";
-    } else if (randNum === 2) {
-        return "paper";
+            let playerSelection = prompt("Chose Rock, Paper, or Scissors: ");
+
+            var playerSelectionLower = playerSelection.toLowerCase();
+    
+            function getComputerChoice() {
+                let randNum = Math.floor(Math.random() * (3 - 1) + 1);
+                if (randNum === 1) {
+                    return "rock";
+                } else if (randNum === 2) {
+                    return "paper";
+                } else {
+                    return "scissors";
+                }
+            };
+    
+            var getComputerChoiceVar = getComputerChoice();
+
+            let pSL = playerSelectionLower;
+            let gCC = getComputerChoiceVar;
+            if ((pSL == "rock" && gCC == "scissors") || (pSL == "paper" && gcc == "rock") || (pSL == "scissors" && gCC == "paper")) {
+                win += 1;
+                return "You win " + pSL + " beats " + gCC + "!";
+            } else if (pSL === gCC) {
+                tie += 1;
+                return "You tied. Both chose " + pSL;
+            } else {
+                loss += 1;
+                return "You lose! " + gCC + " beats " + pSL + ".";
+            }
+        };
+        console.log(gameLogic());
+      };
+    if (win > loss) {
+        return "You Won! Wins: " + win + " Losses: " + loss + " Ties: " + tie;
+    } else if (win === loss) {
+        return "You tied. Wins: " + win + " Losses: " + loss + " Ties: " + tie;
     } else {
-        return "scissors";
+        return "You lost. Wins: " + win + " Losses: " + loss + " Ties: " + tie;
     }
-};
+ };
 
-function gameLogic(playerSelectionLower, getComputerChoice) {
-    let pSL = playerSelectionLower;
-    let gCC = getComputerChoice;
-    if ((pSL == "rock" && gCC == "scissors") || (pSL == "paper" && gcc == "rock") || (pSL == "scissors" && gCC == "paper")) {
-        return "You win " + pSL + " beats " + gCC + "!";
-    } else if (pSL === gCC) {
-
-        return "You tied. Both chose " + pSL;
-    } else {
-        return "You lose! " + gCC + " beats " + pSL + ".";
-    }
-};
+ console.log(game());
