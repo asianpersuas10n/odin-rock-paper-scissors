4519ba1dbafbf9ac0f8f6234f2b5e38e6e29d13d
nick <nicholasjpenaranda@gmail.com>
2022-07-18 01:38:44 -0500

    make function that will return rock paper or scissors

---
+++ rps.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    const randNum = Math.floor(Math.random() * (3 - 1) + 1);
+    if (randNum === 1) {
+        return "Rock";
+    } else if (randNum === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice())
